bc726af2afa8da2c6d404206df871a8ac05d8fef
21CSM <81891917+21CSM@users.noreply.github.com>
2024-09-14 14:45:06 -0400

    build(flake): add tauri dependencies
    closes GH-13

--- flake.nix
+++ flake.nix
@@ -1,6 +1,5 @@
 {
-  description = "Dev environment for Rust based on Nix flakes";
-
+  description = "Dev environment for Rust and Tauri 2.0 based on Nix flakes";
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
@@ -9,26 +8,38 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
-
   outputs = { self, nixpkgs, flake-utils, fenix }:
     flake-utils.lib.eachDefaultSystem (system:
       let
-        pkgs = nixpkgs.legacyPackages.${system};
-        rustToolchain = fenix.packages.${system}.minimal.toolchain;
-      in
-      {
+        overlays = [ fenix.overlays.default (import ./nix/overlays) ];
+        pkgs = import nixpkgs { inherit system overlays; };
+        rustToolchain = pkgs.fenix.stable.withComponents [
+          "cargo" "rustc" "rust-src" "clippy" "rustfmt"
+        ];
+        buildInputs = with pkgs; [
+          rustToolchain openssl cargo-tauri
+        ] ++ lib.optionals stdenv.isLinux [ glib gtk3 webkitgtk libsoup ]
+          ++ lib.optionals stdenv.isDarwin [ darwin.apple_sdk.frameworks.WebKit ];
+      in {
         devShells.default = pkgs.mkShell {
-          buildInputs = [
-            rustToolchain
-          ];
+          inherit buildInputs;
+          shellHook = ''
+            export PATH=${pkgs.lib.makeBinPath buildInputs}:$PATH
+          '';
         };
-
-        checks.default = pkgs.runCommand "check-rust-env" {
-          buildInputs = [ rustToolchain ];
+        checks.default = pkgs.runCommand "check-rust-tauri-env" {
+          inherit buildInputs;
         } ''
-          if ! command -v rustc > /dev/null || ! command -v cargo > /dev/null; then
-            exit 1
-          fi
+          export PATH=${pkgs.lib.makeBinPath buildInputs}:$PATH
+          command -v rustc > /dev/null && command -v cargo > /dev/null || { echo "Rust toolchain not found"; exit 1; }
+          command -v cargo-tauri > /dev/null || { echo "Tauri CLI not found"; exit 1; }
+          cargo_tauri_version=$(cargo-tauri --version)
+          [[ $cargo_tauri_version == tauri-cli* && $cargo_tauri_version == *2.0.0* ]] || { echo "Incorrect Tauri CLI version: $cargo_tauri_version"; exit 1; }
+          ${pkgs.lib.optionalString pkgs.stdenv.isLinux ''
+            pkg-config --exists gtk+-3.0 || { echo "GTK3 not found"; exit 1; }
+            pkg-config --exists webkit2gtk-4.0 || { echo "WebKitGTK not found"; exit 1; }
+          ''}
+          echo "All checks passed"
           touch $out
         '';
       }
